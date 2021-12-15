f6a3a55edad05450411d7da1616485a680d10ba3
Luciano Martins <zicadopv@gmail.com>
2021-12-15 12:04:16 -0300

    Subindo os testes finais

--- project_space_flight/tests/test_api.py
+++ project_space_flight/tests/test_api.py
@@ -1,8 +1,58 @@
-import json
 import uuid
 
 import pytest
 from django.urls import reverse
+
+from voos.models import Voo
+
+dados_artigo = {
+    "key": "3221",
+    "title": "Parsons to develop prototype ground operations center for DARPA’s Blackjack satellites",
+    "url": "https://spacenews.com/parsons-to-develop-prototype-ground/",
+    "image_url": "https://spacenews.com/wp-content/uploads/2020/03/DARPA-470x470.png",
+    "news_site": "SpaceNews",
+    "summary": "The Defense Advanced Research Projects Agency.",
+    "published_at": "2021-12-11T20:30:12.000Z",
+    "updated_at": "2021-12-11T20:30:12.528Z",
+    "featured": "False",
+    "launches": [],
+    "events": []
+}
+
+
+@pytest.fixture
+def artigo(db):
+    voo = Voo(
+        key=1221,
+        title='Parsons to develop prototype ground operations center for DARPA’s Blackjack satellites',
+        url='https://spacenews.com/parsons-to-develop-prototype-ground/',
+        image_url='https://spacenews.com/wp-content/uploads/2020/03/DARPA-470x470.png',
+        news_site='SpaceNews',
+        summary='The Defense Advanced Research Projects Agency.',
+        published_at='2021-12-11T20:30:12.000Z',
+        updated_at='2021-12-11T20:30:12.528Z',
+        featured=False,
+        launches=[],
+        events=[]
+    )
+    voo.save()
+    return voo
+
+
+@pytest.fixture
+def generate_token(client, django_user_model):
+    kwargs = {}
+    kwargs['username'] = "user1"
+    kwargs['password'] = "bar"
+    django_user_model.objects.create_user(**kwargs)
+    payload = kwargs
+    url = reverse('token_obtain_pair')
+    resposta = client.post(
+        url,
+        data=payload,
+        format='json',
+    )
+    return resposta.json()['access']
 
 
 @pytest.fixture
@@ -10,91 +60,45 @@ def resp(client):
     resp = client.get('https://127.0.0.1:8000')
     return resp
 
-@pytest.fixture
-def test_password():
-   return 'strong-test-pass'
-
-
-def popular_banco_api():
-    ...
-
-def test_status_code(resp):
-    assert resp.status_code == 200
-
-
-@pytest.mark.django_db
-def test_list_(client):
-   url = reverse(
-       'article_details', kwargs={'key': '1221'}
-   )
-   response = client.get(url)
-   assert response.status_code == 200
-   assert len(response.json()) > 0
-
 
 @pytest.fixture
-def create_user(db, django_user_model, test_password):
-   def make_user(**kwargs):
-       kwargs['password'] = test_password
-       if 'username' not in kwargs:
-           kwargs['username'] = str(uuid.uuid4())
-       return django_user_model.objects.create_user(**kwargs)
-   return make_user
+def create_user(db, django_user_model):
+    def make_user(**kwargs):
+        kwargs['password'] = '123456'
+        if 'username' not in kwargs:
+            kwargs['username'] = str(uuid.uuid4())
+        return django_user_model.objects.create_user(**kwargs)
+
+    return make_user
 
 
-@pytest.mark.django_db
-def test_user_detail(client, create_user):
-   user = create_user(username='someone')
-   url = reverse('user-detail-view', kwargs={'pk': user.pk})
-   response = client.get(url)
-   assert response.status_code == 200
-   assert 'someone' in user.username
-   assert user is not None
-
-
-@pytest.mark.django_db
-def test_auth_view(client, create_user, test_password):
-   user = create_user()
-   url = reverse('auth-url')
-   client.login(
-       username=user.username,
-       password=test_password
-   )
-   response = client.get(url)
-   assert response.status_code == 200
-
-
-@pytest.mark.django_db
-def test_lista_de_artigos_vazia(client):
+def test_lista_de_artigos(client, artigo):
     url = reverse('articles')
-    retorno = client.get(url)
-    assert retorno.json() == []
+    response = client.get(url)
+    assert len(response.json()) > 0
 
 
-def test_preencher_base_com_dados(db):
-    assert retorno.json() == []
+def test_artigo_presente_no_banco(client, artigo):
+    url = reverse(
+        'article_details', kwargs={'key': '1221'}
+    )
+    response = client.get(url)
+    assert len(response.json()) > 0
 
 
-@pytest.mark.django_db
-def test_generate_token(client, create_user):
-    user = create_user()
-    url = reverse('token_obtain_pair')
-    payload = {'username': user.username, 'password': user.password}
-    assert user is None
-    # assert payload['username'] == 'Anta'
-    # token = client.post(
-    #     url,
-    #     data=payload,
-    #     format='json',
-    # )
-    # print(token.data)
-    # assert token == []
+def test_generate_token(generate_token):
+    assert generate_token is not None
 
-
-def test_columns_exists(resp):
-    columns = ['id', 'title', 'url', 'imageUrl', 'newsSite', 'summary', 'publishedAt',
-               'updatedAt', 'featured', 'launches', 'events']
-    for info in json.loads(resp.text):
-        line = info
-        for index, key in enumerate(line):
-            assert key == columns[index]
+# def test_criar_artigo(client, generate_token):
+#     url = reverse('article_new')
+#     headers = {
+#         'Authorization': f'Bearer {generate_token}',
+#         'Content-Type': 'application/json'
+#     }
+#     response = client.post(
+#         url,
+#         headers=headers,
+#         data=dados_artigo,
+#         format='json',
+#     )
+#     assert response.status_code == 200
